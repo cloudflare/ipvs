cloudflare/ipvs

commit bc6bb040259246926fbacb75f1b7c8d27e834a16

feat: value type for IP netmasks
This changeset is the first in a series to replace this module's IP
address types with those from netip. A value type to represented IP netmasks
following the pattern of netip.Addr, intending to replace the existing
IPMask type.

Unlike net.IPMask, the type introduced by the netmask package is
comparable (you can use `==` and use it as a map key), as well as being
immutable while taking up less memory, especially for IPv6.

Unlike netip.Prefix, this type does not require canonical prefix-based
netmasks for IPv4, nor are Mask directly linked to addresses.

netmask/mask.go
@@ -1,0 +1,311 @@
+// Portions of netmask adapted from the Go Standard Library.
+// Copyright 2020 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the go.LICENSE file.
+
+// Package netmask defiens a value type representing an
+// network mask for IPv4 and IPv6.
+//
+// Compared to the net.IPMask type, this package takes
+// less memory, is immutable, and is comparable.
+package netmask
+
+import (
+	"encoding/binary"
+	"errors"
+	"strconv"
+	"strings"
+)
+
+// Mask represents an IPv4 mask or an IPv6 prefix, similar to net.IPMask or netip.Prefix.
+//
+// Unlike net.IPMask, Mask is a comparable value type (it supports == and can be map key) and
+// is immutable.
+//
+// Unlike netip.Prefix, Mask is not attached to an IP address, and does not require IPv4 masks
+// to be a prefix.
+type Mask struct {
+	// mask
+	mask uint32
+
+	// z is the mask's address family
+	//
+	// 0 means an invalid Mask (the zero Mask)
+	// z4 means an IPv4 address.
+	// z6 means an IPv6 address.
+	z int8
+}
+
+// z0, z4, and z6 are sentinel Mask.z values.
+const (
+	z0 int8 = iota
+	z4
+	z6
+)
+
+// MaskFrom4 returns the IPv4 mask given by the bytes in mask.
+func MaskFrom4(mask [4]byte) Mask {
+	return Mask{
+		mask: uint32(uint32(mask[0])<<24 | uint32(mask[1])<<16 | uint32(mask[2])<<8 | uint32(mask[3])),
+		z:    z4,
+	}
+}
+
+// MaskFrom16 returns the IPv6 prefix given by the prefix in mask.
+// Note that if the prefix is not one bits followed by all zero bits
+// the invalid Mask is returned.
+func MaskFrom16(mask [16]byte) Mask {
+	ones := prefixLength(mask[:])
+	if ones == -1 {
+		return Mask{}
+	}
+
+	return Mask{
+		mask: uint32(ones),
+		z:    z6,
+	}
+}
+
+// MaskFromSlice parses the 4- or 16-byte slices as an IPv4 netmask or IPv6 prefix.
+// Note that a net.IPMask can by passed directly as the []byte argument. IIf slice's
+// length is not 4 or 16, MaskFromSlice returns Mask{}, false.
+func MaskFromSlice(mask []byte) (Mask, bool) {
+	switch len(mask) {
+	case 4:
+		return MaskFrom4(*(*[4]byte)(mask)), true
+	case 16:
+		return MaskFrom16(*(*[16]byte)(mask)), true
+	}
+
+	return Mask{}, false
+}
+
+// prefixLength returns the number of leading one bits in mask. If the prefix isn't
+// followed entirely by zero bits, -1 is returned.
+func prefixLength(mask []byte) int {
+	var n int
+	for i, v := range mask {
+		if v == 0xFF {
+			n += 8
+			continue
+		}
+		for v&0x80 != 0 {
+			n++
+			v <<= 1
+		}
+		if v != 0 {
+			return -1
+		}
+		for i++; i < len(mask); i++ {
+			if mask[i] != 0 {
+				return -1
+			}
+		}
+		break
+	}
+	return n
+}
+
+// AsSlice returns an IPv4 or IPv6 mask in its respective 4-byte or 16-byte representation.
+func (mask Mask) AsSlice() []byte {
+	switch mask.z {
+	case z0:
+		return nil
+	case z4:
+		var ret [4]byte
+		binary.BigEndian.PutUint32(ret[:], mask.mask)
+		return ret[:]
+	default:
+		var ret [16]byte
+		n := uint(mask.mask)
+		for i := 0; i < 16; i++ {
+			if n >= 8 {
+				ret[i] = 0xFF
+				n -= 8
+				continue
+			}
+			ret[i] = ^byte(0xFF >> n)
+			n = 0
+		}
+		return ret[:]
+	}
+}
+
+// MaskFrom returns a Mask consisting of 'ones' 1 bits followed by 0s up to a total length
+// of 'bits' bits.
+func MaskFrom(ones, bits int) Mask {
+	if ones < 0 || ones > bits {
+		return Mask{}
+	}
+
+	switch bits {
+	case 32:
+		var mask [4]byte
+		n := uint(ones)
+		for i := 0; i < 4; i++ {
+			if n >= 8 {
+				mask[i] = 0xFF
+				n -= 8
+				continue
+			}
+			mask[i] = ^byte(0xFF >> n)
+			n = 0
+		}
+		return MaskFrom4(mask)
+	case 128:
+		return Mask{
+			mask: uint32(ones),
+			z:    z6,
+		}
+	default:
+		return Mask{}
+	}
+}
+
+// IsValid reports whether the Mask is an initialized mask (not the zero Mask).
+//
+// Note that a non-prefix mask is considered valid, even for IPv6.
+func (mask Mask) IsValid() bool {
+	return mask.z != z0
+}
+
+// Is4 reports whether the Mask is for IPv4.
+func (mask Mask) Is4() bool {
+	return mask.z == z4
+}
+
+// Is6 reports whether the mask is for IPv6.
+func (mask Mask) Is6() bool {
+	return mask.z == z6
+}
+
+// Bits returns the masks's prefix length.
+//
+// It reports -1 if the mask does not contain a prefix.
+func (mask Mask) Bits() int {
+	switch mask.z {
+	case z0:
+		return -1
+	case z4:
+		mask := mask.AsSlice()
+		return prefixLength(mask)
+	default:
+		return int(mask.mask)
+	}
+}
+
+// MarshalBinary implements the encoding.BinaryMarshaler interface.
+// It returns a zero-length slice for the zero Mask, the 4-byte mask
+// for IPv4, and a 1-byte prefix for IPv6.
+func (mask Mask) MarshalBinary() ([]byte, error) {
+	switch mask.z {
+	case z0:
+		return []byte{}, nil
+	case z4:
+		b := make([]byte, 4)
+		b[0] = uint8(mask.mask >> 24)
+		b[1] = uint8(mask.mask >> 16 & 0xFF)
+		b[2] = uint8(mask.mask >> 8 & 0xFF)
+		b[3] = uint8(mask.mask & 0xFF)
+		return b, nil
+	default:
+		return []byte{byte(mask.mask)}, nil
+	}
+}
+
+// UnmarshalBinary implements the encoding.BinaryUnmarshaler interface. It
+// expects data in the form generated by MarshalBinary.
+func (mask *Mask) UnmarshalBinary(b []byte) error {
+	n := len(b)
+	switch {
+	case n == 0:
+		*mask = Mask{}
+		return nil
+	case n == 4:
+		*mask = MaskFrom4(*(*[4]byte)(b))
+		return nil
+	case n == 1:
+		*mask = MaskFrom(int(b[0]), 128)
+		return nil
+	}
+
+	return errors.New("unexpected slice size")
+}
+
+// MarshalText implements the encoding.TextMarshaler interface. The encoding is
+// the same as returned by String, with one exception: If mask is the zero Mask,
+// the encoding is the empty string.
+func (mask Mask) MarshalText() ([]byte, error) {
+	switch mask.z {
+	case z0:
+		return []byte(""), nil
+	case z4:
+		return textIPv4(mask), nil
+	default:
+		return []byte(strconv.FormatUint(uint64(mask.mask), 10)), nil
+	}
+}
+
+// UnmarshalText implements the encoding.TextUnmarshaler interface. The mask
+// is expected in a form generated by MarshalText.
+func (mask *Mask) UnmarshalText(text []byte) error {
+	n := len(text)
+	switch {
+	case n == 0:
+		*mask = Mask{}
+		return nil
+	case n == 1:
+		*mask = MaskFrom(int(text[0]), 128)
+		return nil
+	case n >= len("1.1.1.1") && n <= len("255.255.255.255"):
+		sub := strings.SplitN(string(text), ".", 4)
+		if len(sub) != 4 {
+			return errors.New("unexpected slice type")
+		}
+
+		var fields [4]uint32
+		for i, s := range sub {
+			f, err := strconv.ParseUint(s, 10, 8)
+			if err != nil {
+				return err
+			}
+			fields[i] = uint32(f)
+		}
+		*mask = Mask{mask: uint32(fields[0]<<24 | fields[1]<<16 | fields[2]<<8 | fields[3]), z: z4}
+		return nil
+	}
+
+	return errors.New("unexpected slice size")
+}
+
+// String returns the string form of the Mask mask. It returns one of these forms:
+//
+// - "invalid Mask", if mask is the zero Mask
+// - IPv4 dotted decimal ("255.255.255.0")
+// - IPv6 prefix ("64")
+func (mask Mask) String() string {
+	switch mask.z {
+	case z0:
+		return "invalid Mask"
+	case z4:
+		return string(textIPv4(mask))
+	default:
+		return strconv.FormatUint(uint64(mask.mask), 10)
+	}
+}
+
+func (x Mask) Equal(y Mask) bool {
+	return x == y
+}
+
+func textIPv4(mask Mask) []byte {
+	b := make([]byte, 0, len("255.255.255.255"))
+	b = strconv.AppendUint(b, uint64(uint8(mask.mask>>24)), 10)
+	b = append(b, '.')
+	b = strconv.AppendUint(b, uint64(uint8(mask.mask>>16)), 10)
+	b = append(b, '.')
+	b = strconv.AppendUint(b, uint64(uint8(mask.mask>>8)), 10)
+	b = append(b, '.')
+	b = strconv.AppendUint(b, uint64(uint8(mask.mask)), 10)
+	return b
+}

netmask/mask_test.go
@@ -1,0 +1,649 @@
+package netmask
+
+import (
+	"testing"
+
+	"gotest.tools/v3/assert"
+)
+
+func TestNetmask_MaskFrom4(t *testing.T) {
+	type testCase struct {
+		name     string
+		mask     [4]byte
+		expected Mask
+	}
+
+	run := func(t *testing.T, tc testCase) {
+		mask := MaskFrom4(tc.mask)
+		assert.Equal(t, mask, tc.expected)
+	}
+
+	testCases := []testCase{
+		{
+			name: "0",
+			mask: [...]byte{0, 0, 0, 0},
+			expected: Mask{
+				mask: 0,
+				z:    z4,
+			},
+		},
+		{
+			name: "8",
+			mask: [...]byte{255, 0, 0, 0},
+			expected: Mask{
+				mask: 0xFF00_0000,
+				z:    z4,
+			},
+		},
+		{
+			name: "24",
+			mask: [...]byte{255, 255, 255, 0},
+			expected: Mask{
+				mask: 0xFFFF_FF00,
+				z:    z4,
+			},
+		},
+		{
+			name: "32",
+			mask: [...]byte{255, 255, 255, 255},
+			expected: Mask{
+				mask: 0xFFFF_FFFF,
+				z:    z4,
+			},
+		},
+		{
+			name: "non-canonical mask",
+			mask: [...]byte{0, 0, 255, 0},
+			expected: Mask{
+				mask: 0x0000_FF00,
+				z:    z4,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			run(t, tc)
+		})
+	}
+}
+
+func TestNetmask_MaskFrom16(t *testing.T) {
+	type testCase struct {
+		name     string
+		mask     [16]byte
+		expected Mask
+	}
+
+	run := func(t *testing.T, tc testCase) {
+		mask := MaskFrom16(tc.mask)
+		assert.Equal(t, mask, tc.expected)
+	}
+
+	testCases := []testCase{
+		{
+			name: "0",
+			mask: [...]byte{
+				0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0,
+			},
+			expected: Mask{
+				mask: 0,
+				z:    z6,
+			},
+		},
+		{
+			name: "128",
+			mask: [...]byte{
+				0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+				0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+			},
+			expected: Mask{
+				mask: 128,
+				z:    z6,
+			},
+		},
+		{
+			name: "96",
+			mask: [...]byte{
+				0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+				0xFF, 0xFF, 0xFF, 0xFF, 0, 0, 0, 0,
+			},
+			expected: Mask{
+				mask: 96,
+				z:    z6,
+			},
+		},
+		{
+			name: "non-prefix",
+			mask: [...]byte{
+				0xFF, 0xFF, 0xFF, 0xFF, 0, 0, 0, 0,
+				0xFF, 0xFF, 0xFF, 0xFF, 0, 0, 0, 0,
+			},
+			expected: Mask{},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			run(t, tc)
+		})
+	}
+}
+
+func TestNemask_MaskFromSlice(t *testing.T) {
+	type testCase struct {
+		name     string
+		mask     []byte
+		expected Mask
+	}
+
+	run := func(t *testing.T, tc testCase) {
+		mask, ok := MaskFromSlice(tc.mask)
+		assert.Assert(t, ok)
+		assert.Equal(t, mask, tc.expected)
+	}
+
+	testCases := []testCase{
+		{
+			name: "IPv4 /0",
+			mask: []byte{0, 0, 0, 0},
+			expected: Mask{
+				mask: 0,
+				z:    z4,
+			},
+		},
+		{
+			name: "IPv4 /24",
+			mask: []byte{255, 255, 255, 0},
+			expected: Mask{
+				mask: 0xFFFF_FF00,
+				z:    z4,
+			},
+		},
+		{
+			name: "IPv6 /128",
+			mask: []byte{
+				0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+				0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+			},
+			expected: Mask{
+				mask: 128,
+				z:    z6,
+			},
+		},
+		{
+			name: "IPv6 /96",
+			mask: []byte{
+				0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+				0xFF, 0xFF, 0xFF, 0xFF, 0, 0, 0, 0,
+			},
+			expected: Mask{
+				mask: 96,
+				z:    z6,
+			},
+		},
+		{
+			name: "non-prefix",
+			mask: []byte{
+				0xFF, 0xFF, 0xFF, 0xFF, 0, 0, 0, 0,
+				0xFF, 0xFF, 0xFF, 0xFF, 0, 0, 0, 0,
+			},
+			expected: Mask{},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			run(t, tc)
+		})
+	}
+}
+
+func TestNemask_AsSlice(t *testing.T) {
+	type testCase struct {
+		name     string
+		mask     Mask
+		expected []byte
+	}
+
+	run := func(t *testing.T, tc testCase) {
+		slice := tc.mask.AsSlice()
+		assert.DeepEqual(t, slice, tc.expected)
+	}
+
+	testCases := []testCase{
+		{
+			name:     "zero Mask",
+			mask:     Mask{},
+			expected: nil,
+		},
+		{
+			name: "IPv4 /0",
+			mask: Mask{
+				mask: 0,
+				z:    z4,
+			},
+			expected: []byte{0, 0, 0, 0},
+		},
+		{
+			name: "IPv4 /24",
+			mask: Mask{
+				mask: 0xFFFF_FF00,
+				z:    z4,
+			},
+			expected: []byte{255, 255, 255, 0},
+		},
+		{
+			name: "IPv6 /128",
+			mask: Mask{
+				mask: 128,
+				z:    z6,
+			},
+			expected: []byte{
+				0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+				0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+			},
+		},
+		{
+			name: "IPv6 /96",
+			mask: Mask{
+				mask: 96,
+				z:    z6,
+			},
+			expected: []byte{
+				0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+				0xFF, 0xFF, 0xFF, 0xFF, 0, 0, 0, 0,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			run(t, tc)
+		})
+	}
+}
+
+func TestNetmask_MaskFrom(t *testing.T) {
+	type testCase struct {
+		name     string
+		ones     int
+		bits     int
+		expected Mask
+	}
+
+	run := func(t *testing.T, tc testCase) {
+		mask := MaskFrom(tc.ones, tc.bits)
+		assert.Equal(t, mask, tc.expected)
+	}
+
+	testCases := []testCase{
+		{
+			name: "IPv4 /0",
+			ones: 0,
+			bits: 32,
+			expected: Mask{
+				mask: 0,
+				z:    z4,
+			},
+		},
+		{
+			name: "IPv4 /24",
+			ones: 24,
+			bits: 32,
+			expected: Mask{
+				mask: 0xFFFF_FF00,
+				z:    z4,
+			},
+		},
+		{
+			name: "IPv6 /128",
+			ones: 128,
+			bits: 128,
+			expected: Mask{
+				mask: 128,
+				z:    z6,
+			},
+		},
+		{
+			name: "IPv6 /96",
+			ones: 96,
+			bits: 128,
+			expected: Mask{
+				mask: 96,
+				z:    z6,
+			},
+		},
+		{
+			name:     "invalid ones",
+			ones:     255,
+			bits:     32,
+			expected: Mask{},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			run(t, tc)
+		})
+	}
+}
+
+func TestNetmask_IsValid(t *testing.T) {
+	type testCase struct {
+		name     string
+		mask     Mask
+		expected bool
+	}
+
+	run := func(t *testing.T, tc testCase) {
+		assert.Equal(t, tc.mask.IsValid(), tc.expected)
+	}
+
+	testCases := []testCase{
+		{
+			name: "IPv4",
+			mask: Mask{
+				mask: 0,
+				z:    z4,
+			},
+			expected: true,
+		},
+		{
+			name: "IPv6",
+			mask: Mask{
+				mask: 128,
+				z:    z6,
+			},
+			expected: true,
+		},
+		{
+			name:     "invalid",
+			mask:     Mask{},
+			expected: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			run(t, tc)
+		})
+	}
+}
+
+func TestNetmask_Is4(t *testing.T) {
+	type testCase struct {
+		name     string
+		mask     Mask
+		expected bool
+	}
+
+	run := func(t *testing.T, tc testCase) {
+		assert.Equal(t, tc.mask.Is4(), tc.expected)
+	}
+
+	testCases := []testCase{
+		{
+			name: "IPv4",
+			mask: Mask{
+				mask: 0,
+				z:    z4,
+			},
+			expected: true,
+		},
+		{
+			name: "IPv6",
+			mask: Mask{
+				mask: 128,
+				z:    z6,
+			},
+			expected: false,
+		},
+		{
+			name:     "invalid",
+			mask:     Mask{},
+			expected: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			run(t, tc)
+		})
+	}
+}
+
+func TestNetmask_Is6(t *testing.T) {
+	type testCase struct {
+		name     string
+		mask     Mask
+		expected bool
+	}
+
+	run := func(t *testing.T, tc testCase) {
+		assert.Equal(t, tc.mask.Is6(), tc.expected)
+	}
+
+	testCases := []testCase{
+		{
+			name: "IPv4",
+			mask: Mask{
+				mask: 0,
+				z:    z4,
+			},
+			expected: false,
+		},
+		{
+			name: "IPv6",
+			mask: Mask{
+				mask: 128,
+				z:    z6,
+			},
+			expected: true,
+		},
+		{
+			name:     "invalid",
+			mask:     Mask{},
+			expected: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			run(t, tc)
+		})
+	}
+}
+
+func TestNetmask_Bits(t *testing.T) {
+	type testCase struct {
+		name     string
+		mask     Mask
+		expected int
+	}
+
+	run := func(t *testing.T, tc testCase) {
+		assert.Equal(t, tc.mask.Bits(), tc.expected)
+	}
+
+	testCases := []testCase{
+		{
+			name: "IPv4",
+			mask: Mask{
+				mask: 0xFFFF_FF00,
+				z:    z4,
+			},
+			expected: 24,
+		},
+		{
+			name: "IPv6",
+			mask: Mask{
+				mask: 128,
+				z:    z6,
+			},
+			expected: 128,
+		},
+		{
+			name:     "invalid",
+			mask:     Mask{},
+			expected: -1,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			run(t, tc)
+		})
+	}
+}
+
+func TestNetmask_MarshalBinary(t *testing.T) {
+	type testCase struct {
+		name     string
+		mask     Mask
+		expected []byte
+	}
+
+	run := func(t *testing.T, tc testCase) {
+		out, err := tc.mask.MarshalBinary()
+		assert.NilError(t, err)
+		assert.DeepEqual(t, out, tc.expected)
+	}
+
+	testCases := []testCase{
+		{name: "zero mask", mask: Mask{}, expected: []byte{}},
+		{name: "ipv4 mask", mask: MaskFrom(31, 32), expected: []byte{0xFF, 0xFF, 0xFF, 0xFE}},
+		{name: "weird ipv4 mask", mask: MaskFrom4([...]byte{0xFF, 0x00, 0xFF, 0x00}), expected: []byte{0xFF, 0x00, 0xFF, 0x00}},
+		{name: "ipv6 mask", mask: MaskFrom(128, 128), expected: []byte{128}},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			run(t, tc)
+		})
+	}
+}
+
+func TestNetmask_UnmarshalBinary(t *testing.T) {
+	type testCase struct {
+		name     string
+		mask     []byte
+		expected Mask
+	}
+
+	run := func(t *testing.T, tc testCase) {
+		out := Mask{}
+		err := out.UnmarshalBinary(tc.mask)
+		assert.NilError(t, err)
+		assert.DeepEqual(t, out, tc.expected)
+	}
+
+	testCases := []testCase{
+		{name: "zero mask", mask: []byte{}, expected: Mask{}},
+		{name: "ipv4 mask", mask: []byte{0xFF, 0xFF, 0xFF, 0xFE}, expected: MaskFrom(31, 32)},
+		{name: "weird ipv4 mask", mask: []byte{0xFF, 0x00, 0xFF, 0x00}, expected: MaskFrom4([...]byte{0xFF, 0x00, 0xFF, 0x00})},
+		{name: "ipv6 mask", mask: []byte{128}, expected: MaskFrom(128, 128)},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			run(t, tc)
+		})
+	}
+}
+
+func TestNetmask_MarshalText(t *testing.T) {
+	type testCase struct {
+		name     string
+		mask     Mask
+		expected string
+	}
+
+	run := func(t *testing.T, tc testCase) {
+		out, err := tc.mask.MarshalText()
+		assert.NilError(t, err)
+		assert.DeepEqual(t, string(out), tc.expected)
+	}
+
+	testCases := []testCase{
+		{name: "zero mask", mask: Mask{}, expected: ""},
+		{name: "ipv4 mask", mask: MaskFrom(31, 32), expected: "255.255.255.254"},
+		{name: "weird ipv4 mask", mask: MaskFrom4([...]byte{0xFF, 0x00, 0xFF, 0x00}), expected: "255.0.255.0"},
+		{name: "ipv6 mask", mask: MaskFrom(128, 128), expected: "128"},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			run(t, tc)
+		})
+	}
+}
+
+func TestNetmask_UnmarshalText(t *testing.T) {
+	type testCase struct {
+		name     string
+		mask     []byte
+		expected Mask
+	}
+
+	run := func(t *testing.T, tc testCase) {
+		out := Mask{}
+		err := out.UnmarshalText(tc.mask)
+		assert.NilError(t, err)
+		assert.DeepEqual(t, out, tc.expected)
+	}
+
+	testCases := []testCase{
+		{name: "zero mask", mask: []byte{}, expected: Mask{}},
+		{name: "ipv4 mask", mask: []byte("255.255.255.254"), expected: Mask{mask: 4294967294, z: z4}},
+		{name: "weird ipv4 mask", mask: []byte("255.0.255.0"), expected: Mask{mask: 4278255360, z: z4}},
+		{name: "ipv6 mask", mask: []byte{56}, expected: Mask{mask: 56, z: z6}},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			run(t, tc)
+		})
+	}
+}
+
+func TestNetmask_String(t *testing.T) {
+	type testCase struct {
+		name     string
+		mask     Mask
+		expected string
+	}
+
+	run := func(t *testing.T, tc testCase) {
+		out := tc.mask.String()
+		assert.DeepEqual(t, out, tc.expected)
+	}
+
+	testCases := []testCase{
+		{name: "zero mask", mask: Mask{}, expected: "invalid Mask"},
+		{name: "ipv4 mask", mask: MaskFrom(31, 32), expected: "255.255.255.254"},
+		{name: "weird ipv4 mask", mask: MaskFrom4([...]byte{0xFF, 0x00, 0xFF, 0x00}), expected: "255.0.255.0"},
+		{name: "ipv6 mask", mask: MaskFrom(128, 128), expected: "128"},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			run(t, tc)
+		})
+	}
+}

netmask/go.LICENSE
@@ -1,0 +1,27 @@
+Copyright (c) 2009 The Go Authors. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+   * Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+   * Redistributions in binary form must reproduce the above
+copyright notice, this list of conditions and the following disclaimer
+in the documentation and/or other materials provided with the
+distribution.
+   * Neither the name of Google Inc. nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.

commit ebf202bb6c249e530a4239c1f8df3ad253898e7f

fix(netmask): text marshaler with multibyte prefix
For the IPv6 family the Mask type stores a prefix, rather than a full
128-bit mask. To implement the TextMarshaller interface for these
prefixes, Mask marshals the number of one-bits in the prefix to a
string.

However, the unmarshal function expected IPv6 family prefixes to only
ever be 1 byte long. This holds true for the BinaryMarshaller
implementation, but in text prefixes larger than 9 would be represented
by multiple bytes, one for each numeric digit.

This implementation adjusts the unmarshal implementation to accept
lengths up to 3 bytes for IPv6.

Fixes: bc6bb04 ("feat: value type for IP netmasks")

go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/xlab/c-for-go v0.0.0-20230906092656-a1822f0a09c1
 	golang.org/x/tools v0.14.0
 	gotest.tools/v3 v3.4.0
+	pgregory.net/rapid v1.1.0
 )
 
 require (

go.sum
@@ -73,3 +73,5 @@ modernc.org/strutil v1.1.3 h1:fNMm+oJklMGYfU9Ylcywl0CO5O6nTfaowNsh2wpPjzY=
 modernc.org/strutil v1.1.3/go.mod h1:MEHNA7PdEnEwLvspRMtWTNnp2nnyvMfkimT1NKNAGbw=
 modernc.org/token v1.0.1 h1:A3qvTqOwexpfZZeyI0FeGPDlSWX5pjZu9hF4lU+EKWg=
 modernc.org/token v1.0.1/go.mod h1:UGzOrNV1mAFSEB63lOFHIpNRUVMvYTc6yu1SMY/XTDM=
+pgregory.net/rapid v1.1.0 h1:CMa0sjHSru3puNx+J0MIAuiiEV4N0qj8/cMWGBBCsjw=
+pgregory.net/rapid v1.1.0/go.mod h1:PY5XlDGj0+V1FCq0o192FdRhpKHGTRIWBgqjDBTrq04=

netmask/mask.go
@@ -254,8 +254,12 @@ func (mask *Mask) UnmarshalText(text []byte) error {
 	case n == 0:
 		*mask = Mask{}
 		return nil
-	case n == 1:
-		*mask = MaskFrom(int(text[0]), 128)
+	case n >= 1 && n <= 3:
+		u, err := strconv.ParseUint(string(text[:]), 10, 64)
+		if err != nil {
+			return err
+		}
+		*mask = MaskFrom(int(u), 128)
 		return nil
 	case n >= len("1.1.1.1") && n <= len("255.255.255.255"):
 		sub := strings.SplitN(string(text), ".", 4)

netmask/mask_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"gotest.tools/v3/assert"
+	"pgregory.net/rapid"
 )
 
 func TestNetmask_MaskFrom4(t *testing.T) {
@@ -610,7 +611,7 @@ func TestNetmask_UnmarshalText(t *testing.T) {
 		{name: "zero mask", mask: []byte{}, expected: Mask{}},
 		{name: "ipv4 mask", mask: []byte("255.255.255.254"), expected: Mask{mask: 4294967294, z: z4}},
 		{name: "weird ipv4 mask", mask: []byte("255.0.255.0"), expected: Mask{mask: 4278255360, z: z4}},
-		{name: "ipv6 mask", mask: []byte{56}, expected: Mask{mask: 56, z: z6}},
+		{name: "ipv6 mask", mask: []byte("56"), expected: Mask{mask: 56, z: z6}},
 	}
 
 	for _, tc := range testCases {
@@ -619,6 +620,31 @@ func TestNetmask_UnmarshalText(t *testing.T) {
 			run(t, tc)
 		})
 	}
+}
+
+func TestNetmask_TextMarshaller(t *testing.T) {
+	rapid.Check(t, func(t *rapid.T) {
+		mask := rapid.Custom[Mask](func(t *rapid.T) Mask {
+			z := rapid.SampledFrom([]int8{z0, z4, z6}).Draw(t, "z")
+
+			switch z {
+			case z4:
+				return MaskFrom(rapid.IntRange(0, 32).Draw(t, "ones"), 32)
+			case z6:
+				return MaskFrom(rapid.IntRange(0, 128).Draw(t, "ones"), 128)
+			default:
+				return Mask{}
+			}
+		}).Draw(t, "mask")
+
+		p, err := mask.MarshalText()
+		assert.NilError(t, err)
+
+		out := Mask{}
+		assert.NilError(t, out.UnmarshalText(p))
+
+		assert.DeepEqual(t, out, mask)
+	})
 }
 
 func TestNetmask_String(t *testing.T) {

netmask/testdata/rapid/TestNetmask_TextMarshaller/TestNetmask_TextMarshaller-20240706211239-1271900.fail
@@ -1,0 +1,9 @@
+# 2024/07/06 21:12:39.000571 [TestNetmask_TextMarshaller] [rapid] draw mask: netmask.Mask{mask:0xa, z:2}
+# 2024/07/06 21:12:39.000578 [TestNetmask_TextMarshaller] assertion failed: error is not nil: unexpected slice size
+# 
+v0.4.8#5215856823522468385
+0x38e38e38e38e4
+0x2
+0x0
+0x9867f1f40f739
+0xa